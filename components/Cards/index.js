49e704a27b61ac194a9847d0914c072d2112d445
EricFerguson76 <turtle0888@gmail.com>
2019-11-01 13:08:32 -0500

    Added card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,71 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    console.log(response.data);
+    response.data.articles.bootstrap.forEach(info => {
+      const newCard1 = newCard(info)
+      createCard.appendChild((newCard1));
+    })
+  })
+  .then(response => {
+    response.data.articles.javascript.forEach(info => {
+      const newCard1 = newCard(info)
+      createCard.appendChild((newCard1));
+    })
+  })
+  .then(response => {
+    response.data.articles.jquery.forEach(info => {
+      const newCard1 = newCard(info)
+      createCard.appendChild((newCard1));
+    })
+  })
+  .then(response => {
+    response.data.articles.node.forEach(info => {
+      const newCard1 = newCard(info)
+      createCard.appendChild((newCard1));
+    })
+  })
+  .then(response => {
+    response.data.articles.technology.forEach(info => {
+      const newCard1 = newCard(info)
+      createCard.appendChild((newCard1));
+    })
+  })
+
+  .catch(error => {
+    console.log('The data was not returned', error);
+  });
+
+function newCard(info) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const cardTitle = document.createElement('div');
+  cardTitle.classList.add('headline');
+  cardTitle.textContent = info.headline;
+
+  const cardAuthor = document.createElement('div');
+  cardAuthor.classList.add('author');
+
+  const imgContainer = document.createElement('div');
+  imgContainer.classList.add('img-container');
+
+  const cardImg = document.createElement('img');
+  cardImg.src = info.authorPhoto;
+
+  const cardName = document.createElement('span');
+  cardName.textContent = `By: ${info.authorName}`;
+
+  card.appendChild(cardTitle);
+  card.appendChild(cardAuthor);
+  cardAuthor.appendChild(imgContainer);
+  imgContainer.appendChild(cardImg);
+  cardAuthor.appendChild(cardName);
+
+  return card;
+}
+const createCard = document.querySelector('.cards-container');
